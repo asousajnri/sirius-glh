f5767a03ed9c9736599f01348550ee7a8531095a
Antônio <desenvolvedor02@hublocal.com.br>
2022-03-27 14:05:03 -0300

    Feat: view contract with checklist

--- src/container/modals/view-contract-container-styles.js
+++ src/container/modals/view-contract-container-styles.js
@@ -2,12 +2,19 @@ import styled from 'styled-components';
 import {  Dialog } from '@mui/material'
 
 export const StyledViewContractContainer = styled(Dialog)`
+  .checklist-section {
+    margin-bottom: 10px;
+    font-size: 40px;
+  }
 `;
 
 export const StyledBasicInfosWrap = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
   row-gap: 20px;
+  background: #ded;
+  padding: 20px;
+  margin-bottom: 30px;
 `;
 
 export const StyledBasicInfo = styled.span`
@@ -34,15 +41,34 @@ export const StyledChecklist = styled.ul`
 export const StyledSignatures = styled.li`
   display: flex;
   flex-direction: column;
+  margin-bottom: 20px;
 
   h3 {
     font-size: 20px;
     font-weight: 600;
+    margin-bottom: 15px;
+  }
+`;
+
+export const StyledSignaturesWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  @media (min-width: 600px) {
+    flex-direction: row;
   }
 `;
 
 export const StyledSignature = styled.div`
   display: flex;
+  align-items: center;
   padding: 10px;
   background: #ccc;
+  flex: 1;
+  font-weight: bold;
+
+  &:nth-child(even) {
+    background: #eec;
+  }
 `;
--- src/container/modals/view-contract-container.jsx
+++ src/container/modals/view-contract-container.jsx
@@ -3,6 +3,7 @@ import { DialogTitle, Checkbox, DialogContent, DialogActions } from '@mui/materi
 import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from '@mui/material';
 import axios from 'axios';
+import { isEmpty, isNil } from 'ramda';
 
 import { format } from 'date-fns';
 
@@ -17,6 +18,7 @@ import {
   StyledChecklist,
   StyledSignature,
   StyledSignatures,
+  StyledSignaturesWrap,
 } from './view-contract-container-styles';
 
 export const ViewContractContainer = () => {
@@ -30,7 +32,11 @@ export const ViewContractContainer = () => {
   } = useContractContext();
 
   const getContract = React.useCallback(async () => {
+    console.log(contractId);
+
     try {
+      if(!contractId) return;
+
       const { data } = await axios.get(`http://localhost:3006/contracts/${contractId}`);
       setContract(data);
     } catch (err) {
@@ -60,43 +66,69 @@ export const ViewContractContainer = () => {
         style={{ 
           justifyContent: 'space-between',
           display: 'flex',
+          alignItems: 'center',
         }}
       >
         {contract?.contract_type?.title} - {contract?.provider?.name}
 
-        <IconButton 
+        <IconButton
+          style={{
+            width: "40px",
+            height: "40px",
+            marginLeft: '20px'
+          }} 
           title='Fechar'
           onClick={handleCloseModal}
-          style={{
-            marginLeft: '20px'
-          }}
         >
           <CloseIcon />
         </IconButton>
       </DialogTitle>
-
+        
       <DialogContent dividers>
-        <StyledBasicInfosWrap>
-          <StyledBasicInfo>
-            <label className='label' htmlFor="">Data de início</label>
-            <span className='info'>{format(new Date(contract.startDate), "dd-MM-yyyy")}</span>
-          </StyledBasicInfo>
-          <StyledBasicInfo>
-            <label className='label' htmlFor="">Data de termino</label>
-            <span className='info'>{format(new Date(contract.startDate), "dd-MM-yyyy")}</span>
-          </StyledBasicInfo>
-          <StyledBasicInfo>
-            <label className='label' htmlFor="">Fornecedor</label>
-            <span className='info'>{contract.provider.name}</span>
-          </StyledBasicInfo>
-        </StyledBasicInfosWrap>
-        <StyledChecklist>
-          {contract.checklist_contract.map(checklist => (
-            <StyledSignatures>
-              <h3>{checklist.item}</h3>
-            </StyledSignatures>
-          ))}
-        </StyledChecklist>
+        {!isEmpty(contract) && (
+          <>
+            <StyledBasicInfosWrap>
+              <StyledBasicInfo>
+                <label className='label' htmlFor="">Data de início</label>
+                <span className='info'>{format(new Date(contract?.startDate), "dd-MM-yyyy")}</span>
+              </StyledBasicInfo>
+              <StyledBasicInfo>
+                <label className='label' htmlFor="">Data de termino</label>
+                <span className='info'>{format(new Date(contract?.startDate), "dd-MM-yyyy")}</span>
+              </StyledBasicInfo>
+              <StyledBasicInfo>
+                <label className='label' htmlFor="">Fornecedor</label>
+                <span className='info'>{contract?.provider?.name}</span>
+              </StyledBasicInfo>
+            </StyledBasicInfosWrap>
+
+            <h2 className='checklist-section'>Checklist</h2>
+
+            <StyledChecklist>
+              {contract?.checklist_contract.map((checklist, index) => (
+                <StyledSignatures>
+                  <h3>Check {index} - {checklist?.item}</h3>
+                  <StyledSignaturesWrap>
+                    <StyledSignature>
+                      <Checkbox 
+                        checked={checklist.provider.isChecked}
+                        onChange={() => {}}
+                      />
+                      Fornecedor
+                    </StyledSignature>
+                    <StyledSignature>
+                      <Checkbox 
+                        checked={checklist.recipient.isChecked}
+                        onChange={() => {}}
+                      />
+                      Beneficiário 
+                    </StyledSignature>
+                  </StyledSignaturesWrap>
+                </StyledSignatures>
+              ))}
+            </StyledChecklist>
+          </>
+        )}
       </DialogContent>
       <DialogActions>
       </DialogActions>
